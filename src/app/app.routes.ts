7c37a840a794acd78e45f16c761a41bdf5e99ea6
Juan David Osorio Ortiz <jdoo1115@gmail.com>
2023-05-16 23:15:49 -0500

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('src/app/public/home/home.component')
+      .then(c => c.HomeComponent)
+  },
+  {
+    path: 'user',
+    children: [
+      {
+        path: 'list',
+        loadComponent: () => import('src/app/user/list/list.component')
+          .then(c => c.ListComponent)
+      },
+      {
+        path: ':id',
+        loadComponent: () => import('src/app/user/user/user.component')
+          .then(c => c.UserComponent)
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
