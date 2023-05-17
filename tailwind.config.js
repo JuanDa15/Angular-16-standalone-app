9e4a5f667223ba950dac66d61adabcd719f4a1c9
Juan David Osorio Ortiz <jdoo1115@gmail.com>
2023-05-16 23:12:59 -0500

    install tailwind and daisyUi

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export const prefix = '';
+export const purge = {
+  content: [
+    './src/**/*.{html,ts}',
+  ]
+};
+export const darkMode = 'class';
+export const theme = {
+  extend: {},
+};
+export const variants = {
+  extend: {},
+};
+export const plugins = [];
