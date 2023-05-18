c7617c2a3fdb1b7775999b8d78ec3555ae932759
Juan David Osorio Ortiz <jdoo1115@gmail.com>
2023-05-17 20:25:49 -0500

    add store devtools

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,15 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
+import { AppReducer } from './store/reducers';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideStore()
+    provideStore(AppReducer),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
 ]
 };
